5c693e912dc0236799ef27f467fe432f7750bd57
Jeroen Ooms <jeroen@berkeley.edu>
2019-02-28 16:50:03 +0000

    version 1.0

---
+++ R/qpdf.R
@@ -0,0 +1,101 @@
+#' Split, Combine and Compress PDF Files
+#'
+#' Content-preserving transformations transformations of PDF files. Note qpdf does
+#' not read actual content from PDF files: to extract text and data you need the
+#' pdftools package.
+#'
+#' Currently the package provides the following wrappers:
+#'
+#' - [pdf_length]: show the number of pages in a pdf
+#' - [pdf_split]: split a single pdf into separate files, one for each page
+#' - [pdf_subset]: create a new pdf with a subset of the input pages
+#' - [pdf_combine]: join several pdf files into one
+#' - [pdf_compress]: compress or linearize a pdf file
+#'
+#' These functions to not modify the `input` file: they create new output file(s)
+#' and return the path(s) to these newly created files.
+#'
+#' @export
+#' @name qpdf
+#' @rdname qpdf
+#' @useDynLib qpdf
+#' @importFrom Rcpp sourceCpp
+#' @importFrom askpass askpass
+#' @param input path or url to the input pdf file
+#' @param output base path of the output file(s)
+#' @param password string with password to open pdf file
+#' @examples # extract some pages
+#' pdf_file <- file.path(tempdir(), "output.pdf")
+#' pdf_subset('https://cran.r-project.org/doc/manuals/r-release/R-intro.pdf',
+#'   pages = 1:3, output = pdf_file)
+#' pdf_length(pdf_file)
+#' unlink(pdf_file)
+pdf_split <- function(input, output = NULL, password = ""){
+  input <- get_input(input)
+  if(!length(output))
+    output <- sub("\\.pdf$", "", input)
+  cpp_pdf_split(input, output, password)
+}
+
+#' @export
+#' @rdname qpdf
+pdf_length <- function(input, password = ""){
+  input <- get_input(input)
+  cpp_pdf_length(input, password)
+}
+
+#' @export
+#' @rdname qpdf
+#' @param pages a vector with page numbers so select. Negative numbers
+#' means removing those pages (same as R indexing)
+pdf_subset <- function(input, pages = 1, output = NULL, password = ""){
+  input <- get_input(input)
+  if(!length(output))
+    output <- sub("\\.pdf$", "_output.pdf", input)
+  output <- normalizePath(output, mustWork = FALSE)
+  size <- pdf_length(input)
+  pages <- seq_len(size)[pages]
+  if(any(is.na(pages)) || !length(pages))
+    stop("Selected pages out of range")
+  cpp_pdf_select(input, output, pages, password)
+}
+
+#' @export
+#' @rdname qpdf
+pdf_combine <- function(input, output = NULL, password = ""){
+  input <- get_input_multi(input)
+  if(!length(output))
+    output <- sub("\\.pdf$", "_combined.pdf", input[1])
+  output <- normalizePath(output, mustWork = FALSE)
+  cpp_pdf_combine(input, output, password)
+}
+
+#' @export
+#' @rdname qpdf
+#' @param linearize enable pdf linearization (streamable pdf)
+pdf_compress <- function(input, output = NULL, linearize = FALSE, password = ""){
+  input <- get_input(input)
+  if(!length(output))
+    output <- sub("\\.pdf$", "_output.pdf", input)
+  output <- normalizePath(output, mustWork = FALSE)
+  cpp_pdf_compress(input, output, linearize, password)
+}
+
+password_callback <- function(...){
+  paste(askpass::askpass(...), collapse = "")
+}
+
+get_input <- function(path){
+  if(length(path) != 1)
+    stop("input should contain exactly one file")
+  if(grepl("^https?://", path)){
+    tmp <- file.path(tempdir(), basename(path))
+    curl::curl_download(path, tmp)
+    path <- tmp
+  }
+  normalizePath(path, mustWork = TRUE)
+}
+
+get_input_multi <- function(path){
+  vapply(path, get_input, character(1))
+}
---
+++ src/bindings.cpp
@@ -0,0 +1,105 @@
+#include <qpdf/QPDF.hh>
+#include <qpdf/QPDFPageDocumentHelper.hh>
+#include <qpdf/QPDFWriter.hh>
+#include <qpdf/QUtil.hh>
+#include <string>
+#include <Rcpp.h>
+
+static QPDF read_pdf_with_password(char const* infile, char const* password){
+  QPDF pdf;
+  try {
+    pdf.processFile(infile, password);
+  } catch(const std::exception& e){
+    if (strlen(password) == 0 && strstr(e.what(), "password") != NULL) {
+      Rcpp::Function askpass = Rcpp::Environment::namespace_env("qpdf")["password_callback"];
+      Rcpp::String value = askpass("Please enter password to open PDF file");
+
+      //this is only for testing the new password */
+      QPDF pdf2;
+      pdf2.processFile(infile, value.get_cstring());
+
+      //actually read it
+      pdf.processFile(infile, value.get_cstring());
+    } else {
+      throw;
+    }
+  }
+  return pdf;
+}
+
+// [[Rcpp::export]]
+int cpp_pdf_length(char const* infile, char const* password){
+  QPDF pdf = read_pdf_with_password(infile, password);
+  QPDFObjectHandle root = pdf.getRoot();
+  QPDFObjectHandle pages = root.getKey("/Pages");
+  QPDFObjectHandle count = pages.getKey("/Count");
+  return count.getIntValue();
+}
+
+// [[Rcpp::export]]
+Rcpp::CharacterVector cpp_pdf_split(char const* infile, std::string outprefix, char const* password){
+  QPDF inpdf = read_pdf_with_password(infile, password);
+  std::vector<QPDFPageObjectHelper> pages =  QPDFPageDocumentHelper(inpdf).getAllPages();
+  Rcpp::CharacterVector output(pages.size());
+  for (int i = 0; i < pages.size(); i++) {
+    std::string outfile = outprefix + "_" + QUtil::int_to_string(i+1, pages.size()) + ".pdf";
+    output.at(i) = outfile;
+    QPDF outpdf;
+    outpdf.emptyPDF();
+    QPDFPageDocumentHelper(outpdf).addPage(pages.at(i), false);
+    QPDFWriter outpdfw(outpdf, outfile.c_str());
+    outpdfw.setStaticID(true); // for testing only
+    outpdfw.setStreamDataMode(qpdf_s_preserve);
+    outpdfw.write();
+  }
+  return output;
+}
+
+// [[Rcpp::export]]
+Rcpp::CharacterVector cpp_pdf_select(char const* infile, char const* outfile,
+                                     Rcpp::IntegerVector which, char const* password){
+  QPDF inpdf = read_pdf_with_password(infile, password);
+  std::vector<QPDFPageObjectHelper> pages =  QPDFPageDocumentHelper(inpdf).getAllPages();
+  QPDF outpdf;
+  outpdf.emptyPDF();
+  for (int i = 0; i < which.size(); i++) {
+    int index = which.at(i) -1; //zero index
+    QPDFPageDocumentHelper(outpdf).addPage(pages.at(index), false);
+  }
+  QPDFWriter outpdfw(outpdf, outfile);
+  outpdfw.setStaticID(true); // for testing only
+  outpdfw.setStreamDataMode(qpdf_s_preserve);
+  outpdfw.write();
+  return outfile;
+}
+
+// [[Rcpp::export]]
+Rcpp::CharacterVector cpp_pdf_combine(Rcpp::CharacterVector infiles, char const* outfile,
+                                      char const* password){
+  QPDF outpdf;
+  outpdf.emptyPDF();
+  for (int i = 0; i < infiles.size(); i++) {
+    QPDF inpdf = read_pdf_with_password(infiles.at(i), password);
+    std::vector<QPDFPageObjectHelper> pages =  QPDFPageDocumentHelper(inpdf).getAllPages();
+    for (int i = 0; i < pages.size(); i++) {
+      QPDFPageDocumentHelper(outpdf).addPage(pages.at(i), false);
+    }
+  }
+  QPDFWriter outpdfw(outpdf, outfile);
+  outpdfw.setStaticID(true); // for testing only
+  outpdfw.setStreamDataMode(qpdf_s_preserve);
+  outpdfw.write();
+  return outfile;
+}
+
+// [[Rcpp::export]]
+Rcpp::CharacterVector cpp_pdf_compress(char const* infile, char const* outfile,
+                                       bool linearize, char const* password){
+  QPDF inpdf = read_pdf_with_password(infile, password);
+  QPDFWriter outpdfw(inpdf, outfile);
+  outpdfw.setStaticID(true); // for testing only
+  outpdfw.setStreamDataMode(qpdf_s_compress);
+  outpdfw.setLinearization(linearize);
+  outpdfw.write();
+  return outfile;
+}
